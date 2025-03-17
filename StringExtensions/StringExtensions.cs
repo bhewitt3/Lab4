e0030b4a215ce9d313636253fb224b47ee818b59
bhewitt3 <blake.hewitt@outlook.com>
2025-03-17 12:56:37 -0400

    Initial commit

---
+++ StringExtensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace StringExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+            return char.IsUpper(str[0]);
+        }
+    }
+}
